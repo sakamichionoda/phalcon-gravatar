fc7b69c7f23fbed204dae8160b2481b586997e9d
simon <simon@ministrycrm.com>
2014-12-30 16:21:09 -0600

    first version

---
+++ examples/example1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gravatar library example</title>
+</head>
+<body>
+<form method="post">
+
+    <input type="email" name="email"/>
+
+</form>
+</body>
+</html>
